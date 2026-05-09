6514eb7e76646c9c6b2ee33f106c8247beadf2f5
ch1nazec <zaicev.casha10@gmail.com>
2026-05-10 01:05:34 +1000

    add routes

---
+++ backend/app/routes/__init__.py
@@ -0,0 +1,6 @@
+from .products import router as products_router
+from .cart import router as cart_router
+from .categories import router as categories_router
+
+
+__all__ = ["products_router", "cart_router", "categories_router"]
